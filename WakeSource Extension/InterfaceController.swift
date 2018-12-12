33656d76531b3d08adcdcf52c9b524839758aab8
Vladyslav Yemets <vyemets@cogniance.com>
2018-12-12 13:17:34 +0200

    added watchOS target

---
+++ WakeSource Extension/InterfaceController.swift
@@ -0,0 +1,35 @@
+//
+//  InterfaceController.swift
+//  WakeSource Extension
+//
+//  Created by Vladyslav Yemets on 12/12/18.
+//  Copyright © 2018 Vladyslav Yemets. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+    
+    override func didAppear() {
+        
+    }
+
+}

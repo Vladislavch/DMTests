d49aca147b9dfdcf71675b0da9aa35877d414409
Vladyslav Yemets <chvvvladis@gmail.com>
2018-12-14 17:05:45 +0200

    cahnged UI

--- DMTests/ViewController.swift
+++ DMTests/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
     }
     
     var rootView: CustomView {
